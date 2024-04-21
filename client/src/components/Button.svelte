b0abda44ec91e022793155cc605756e5045f8b18
Shamil Siddique <shamilsdq@live.com>
2024-04-21 15:21:56 +0530

    Created custom button component

---
+++ client/src/components/Button.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let label = "Button";
+  export let size = "regular";
+  export let style = "primary";
+  export let onClick = undefined;
+
+  const STYLE = {
+    primary: "text-white bg-blue-700 hover:bg-blue-800",
+  };
+
+  const SIZE = {
+    large: "text-lg px-5 py-2 rounded-lg",
+    regular: "text-base px-4 py-1.5 rounded-md",
+    small: "text-sm px-3 py-1 rounded-sm",
+  };
+
+  const commonStr = "inline-flex items-center";
+  const styleStr = STYLE[style ?? "primary"];
+  const sizeStr = SIZE[size ?? "regular"];
+</script>
+
+<button on:click={onClick} class={`${commonStr} ${styleStr} ${sizeStr}`}>
+  {label}
+</button>
