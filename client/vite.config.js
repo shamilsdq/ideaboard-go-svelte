17eed74eccfd89bb17c3b343f356823ca6d0224d
Shamil Siddique <shamilsdq@live.com>
2024-04-22 01:45:29 +0530

    Added dotenv to client, and loaded env variables in svelte app

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import * as path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +11,5 @@ export default defineConfig({
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
+  define: { "process.env": process.env },
 });
