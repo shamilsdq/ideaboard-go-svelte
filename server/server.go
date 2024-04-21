10091b4842f45d80188a60e564f65128c870901c
Shamil Siddique <shamilsdq@live.com>
2024-04-21 13:31:36 +0530

    Added routing to use reverse proxy or asset handler for client routes

--- server/server.go
+++ server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 
 	"github.com/gorilla/handlers"
@@ -10,5 +12,16 @@ import (
 
 func NewServer() http.Handler {
 	router := mux.NewRouter()
+	router.PathPrefix("/").Handler(GetAssetServer()).Methods(http.MethodGet)
 	return handlers.LoggingHandler(os.Stdout, router)
+}
+
+func GetAssetServer() http.Handler {
+	if os.Getenv("DEBUG") == "true" {
+		frontendServerUrl := &url.URL{Host: os.Getenv("CLIENT_SERVER_URL")}
+		return httputil.NewSingleHostReverseProxy(frontendServerUrl)
+	} else {
+		rootPath := os.Getenv("CLIENT_FS_ROOT")
+		return &AssetHandler{rootPath: rootPath}
+	}
 }
