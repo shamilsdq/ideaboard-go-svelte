c76eae2f4597192db2c83a94326a7ace7a38ae65
Shamil Siddique <shamilsdq@live.com>
2024-04-21 17:10:17 +0530

    Created component to define sections for a new board

---
+++ client/src/routes/New/BoardSections.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Button from "@/components/Button.svelte";
+  import Input from "@/components/Input.svelte";
+
+  export let items = [];
+
+  const addItem = () => {
+    items = [...items, "New Section"];
+  };
+
+  const removeItem = (idx) => {
+    items = [...items.slice(0, idx), ...items.slice(idx + 1)];
+  };
+</script>
+
+<ul class="list-none flex flex-col gap-3 w-full">
+  {#each items as _item, idx}
+    <li class="flex gap-2">
+      <Input bind:value={items[idx]} extraClass="flex-1" />
+      <Button
+        label="Remove"
+        onClick={() => removeItem(idx)}
+        disabled={items.length === 1}
+      />
+    </li>
+  {/each}
+</ul>
+<Button label="Add" onClick={addItem} />
