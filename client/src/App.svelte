42f2bf11d1bf457164c3bc0d3a8c0ee94979cd02
Shamil Siddique <shamilsdq@live.com>
2024-04-21 14:58:47 +0530

    Setup Home component and added routing

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import { Router, Route } from "svelte-routing";
+  import Home from "@/routes/Home.svelte";
 </script>
 
 <Router>
+  <Route path="/" component={Home} />
   <Route>Not found</Route>
 </Router>
---
+++ client/src/routes/Home.svelte
@@ -0,0 +1,24 @@
+<div class="w-screen min-h-screen flex justify-center items-center">
+  <main class="flex flex-col gap-10 max-w-4xl">
+    <h2 class="text-5xl font-app">Idea Board for teams</h2>
+
+    <div class="flex gap-7">
+      <section
+        class="border flex-1 flex flex-col justify-between items-start gap-5 p-7 bg-gray-100 rounded-md"
+      >
+        <p class="leading-relaxed">
+          Create a custom board for your team. Share the link with your team and
+          start your digital brainstorming session.
+        </p>
+      </section>
+      <section
+        class="border flex-1 flex flex-col justify-between items-start gap-5 p-7 bg-gray-100 rounded-md"
+      >
+        <p class="leading-relaxed">
+          Be an active team player. Join your team in their brainstorming
+          session and pitch in your ideas and opinions.
+        </p>
+      </section>
+    </div>
+  </main>
+</div>
