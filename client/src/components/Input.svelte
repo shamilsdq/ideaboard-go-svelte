adae440675112b81059a00282e6596db3553e994
Shamil Siddique <shamilsdq@live.com>
2024-04-21 15:22:08 +0530

    Created custom input component

---
+++ client/src/components/Input.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let value = "";
+  export let size = "regular";
+
+  const SIZE = {
+    large: "text-lg px-3.5 py-2 rounded-lg",
+    regular: "text-base px-3 py-1.5 rounded-md",
+    small: "text-sm px-2.5 py-1 rounded-sm",
+  };
+
+  const commonStr = "inline-flex items-center ";
+  const styleStr = "bg-white border";
+  const sizeStr = SIZE[size ?? "regular"];
+</script>
+
+<input type="text" bind:value class={`${commonStr} ${styleStr} ${sizeStr}`} />
