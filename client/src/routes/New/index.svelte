511a5a545ee1f0a56acda6370c796bd03c19cdf5
Shamil Siddique <shamilsdq@live.com>
2024-04-21 17:11:01 +0530

    Added component with fields to create new board

---
+++ client/src/routes/New/index.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Button from "@/components/Button.svelte";
+  import Input from "@/components/Input.svelte";
+  import BoardSections from "./BoardSections.svelte";
+
+  let title = "";
+  let sections = ["Section"];
+
+  const onSubmit = () => {
+    console.log(title, sections);
+  };
+</script>
+
+<div class="w-screen min-h-screen flex justify-center items-center py-16">
+  <div class="flex flex-col gap-5 max-w-4xl">
+    <section class="mb-8">
+      <h1 class="text-5xl font-app">New Board</h1>
+    </section>
+
+    <section class="flex flex-col gap-3 p-8 rounded border bg-gray-100">
+      <h3 class="text-xl">Board Title</h3>
+      <Input extraClass="min-w-96" bind:value={title} />
+    </section>
+
+    <section
+      class="flex flex-col gap-3 items-start p-8 rounded border bg-gray-100"
+    >
+      <h3 class="text-xl">Board Sections</h3>
+      <BoardSections bind:items={sections} />
+    </section>
+
+    <section class="mt-2">
+      <Button
+        label="Create Room"
+        onClick={onSubmit}
+        disabled={title === "" || sections.length === 0}
+        size="large"
+      />
+    </section>
+  </div>
+</div>
