0bb98b9f4fa7e743eb932d7fbb4c36925fd2e815
Shamil Siddique <shamilsdq@live.com>
2024-04-21 15:22:28 +0530

    Added Input and Button in Home component

--- client/src/routes/Home.svelte
+++ client/src/routes/Home.svelte
@@ -1,3 +1,8 @@
+<script>
+  import Button from "@/components/Button.svelte";
+  import Input from "@/components/Input.svelte";
+</script>
+
 <div class="w-screen min-h-screen flex justify-center items-center">
   <main class="flex flex-col gap-10 max-w-4xl">
     <h2 class="text-5xl font-app">Idea Board for teams</h2>
@@ -10,6 +15,7 @@
           Create a custom board for your team. Share the link with your team and
           start your digital brainstorming session.
         </p>
+        <Button label="Create New Board" />
       </section>
       <section
         class="border flex-1 flex flex-col justify-between items-start gap-5 p-7 bg-gray-100 rounded-md"
@@ -18,6 +24,10 @@
           Be an active team player. Join your team in their brainstorming
           session and pitch in your ideas and opinions.
         </p>
+        <div class="flex gap-2">
+          <Input />
+          <Button label="Join Board" />
+        </div>
       </section>
     </div>
   </main>
