a96f71f456ad24451a8015a5fd7e76ae7a803e45
Shamil Siddique <shamilsdq@live.com>
2024-04-28 18:48:32 +0530

    Setup and added CORS middleware to the router

--- server/server.go
+++ server/server.go
@@ -11,7 +11,25 @@ import (
 
 func NewServer() http.Handler {
 	router := mux.NewRouter()
-	router.PathPrefix("/api/boards").Handler(routers.GetBoardRouter())
+
+	router.Use(corsMiddleware)
+
 	router.PathPrefix("/").Handler(routers.GetAssetRouter()).Methods(http.MethodGet)
+	router.PathPrefix("/api").Handler(routers.GetBoardRouter())
+
 	return handlers.LoggingHandler(os.Stdout, router)
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS,PUT")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
