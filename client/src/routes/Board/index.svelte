fde14d62458d4b2af575bd1bc1a0560538ee1fea
Shamil Siddique <shamilsdq@live.com>
2024-05-11 17:29:15 +0530

    Created board component which connects to socket using board id

---
+++ client/src/routes/Board/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  import boardsService from "@/services/boards";
+  import { onMount } from "svelte";
+
+  export let boardId;
+
+  let socket = undefined;
+
+  onMount(async () => {
+    socket = await boardsService.connectSocket(boardId);
+  });
+</script>
+
+<div>Board</div>
