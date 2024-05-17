d82f161404941340a9dabe311a4cbd66a9edac0e
Shamil Siddique <shamilsdq@live.com>
2024-05-17 11:55:10 +0530

    Updated message handler to perform actions on board

--- server/utils/message.go
+++ server/utils/message.go
@@ -12,18 +12,49 @@ func HandleIncomingMessages(conn *websocket.Conn, board *entities.Board) {
 	defer conn.Close()
 	for {
 		var dto dtos.BoardSocketDto
-		if err := conn.ReadJSON(&dto); err != nil {
-			fmt.Println("Error reading message: ", err)
-			return
+		if jsonErr := conn.ReadJSON(&dto); jsonErr != nil {
+			continue
 		}
+
+		var err error
 
 		switch dto.Code {
 		case "POST_CREATE":
-			fmt.Println("Post create request")
+			err = handlePostCreate(dto.Content, board)
 		case "POST_UPDATE":
-			fmt.Println("Post update request")
+			err = handlePostUpdate(dto.Content, board)
 		case "POST_DELETE":
-			fmt.Println("Post delete request")
+			err = handlePostDelete(dto.Content, board)
+		default:
+			err = fmt.Errorf("unknown request type: %s", dto.Code)
+		}
+
+		if err != nil {
+			// send json error message to conn
 		}
 	}
+}
+
+func handlePostCreate(content interface{}, board *entities.Board) error {
+	var dto dtos.CreatePostDto
+	if parseErr := ParseToDto(content, &dto); parseErr != nil {
+		return parseErr
+	}
+	return board.AddPost(dto.SectionId, dto.Content)
+}
+
+func handlePostUpdate(content interface{}, board *entities.Board) error {
+	var dto dtos.UpdatePostDto
+	if parseErr := ParseToDto(content, &dto); parseErr != nil {
+		return parseErr
+	}
+	return board.UpdatePost(dto.Id, dto.SectionId, dto.Content)
+}
+
+func handlePostDelete(content interface{}, board *entities.Board) error {
+	var dto dtos.DeletePostDto
+	if parseErr := ParseToDto(content, &dto); parseErr != nil {
+		return parseErr
+	}
+	return board.DeletePost(dto.Id)
 }
