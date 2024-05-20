c3dbf00515a0ed439db68f3104411f0156be0d00
Shamil Siddique <shamilsdq@live.com>
2024-05-21 03:26:29 +0530

    Added method to parse to DTO and validate the DTO instance

--- server/utils/parser.go
+++ server/utils/parser.go
@@ -3,21 +3,35 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
 
 func ParseRequestBody(r *http.Request, dto any) error {
 	return json.NewDecoder(r.Body).Decode(dto)
 }
 
-func ParseToDto(content interface{}, dto interface{}) error {
+func ParseAndValidate(content any, targetDto any) []string {
 	jsonBytes, jsonErr := json.Marshal(content)
 	if jsonErr != nil {
-		return jsonErr
+		return []string{jsonErr.Error()}
 	}
 
-	if parseErr := json.Unmarshal(jsonBytes, dto); parseErr != nil {
-		return parseErr
+	if parseErr := json.Unmarshal(jsonBytes, targetDto); parseErr != nil {
+		return []string{parseErr.Error()}
+	}
+
+	if validationErr := validator.New().Struct(targetDto); validationErr != nil {
+		errors := make([]string, 0)
+		for _, err := range validationErr.(validator.ValidationErrors) {
+			errors = append(errors, generateErrorString(err))
+		}
+		return errors
 	}
 
 	return nil
+}
+
+func generateErrorString(err validator.FieldError) string {
+	return ""
 }
