78f400f7be63b5464cf76a44ff8abdd1f207eb97
Shamil Siddique <shamilsdq@live.com>
2024-05-23 00:53:23 +0530

    Setup Loader component

---
+++ client/src/components/Loader.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let style = "primary";
+  export let size = "regular";
+
+  const STYLE = {
+    primary: "border-gray-300 border-t-blue-600",
+  };
+
+  const SIZE = {
+    large: "h-20 w-20 border-8",
+    regular: "h-14 w-14 border-8",
+    small: "h-8 w-8 border-4",
+  };
+
+  const commonStr = "rounded-full animate-spin";
+  const sizeStr = SIZE[size ?? "regular"];
+  const styleStr = STYLE[style ?? "primary"];
+</script>
+
+<div class={`${commonStr} ${styleStr} ${sizeStr}`} />
