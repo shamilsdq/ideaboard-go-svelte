95bc566de1a2ce7f954d6fa88de408fff8639424
Shamil Siddique <shamilsdq@live.com>
2024-07-13 02:45:10 +0530

    Created header component for board page

---
+++ client/src/routes/Board/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { boardStore } from "@/stores/board";
+</script>
+
+<header class="w-full flex justify-between items-center gap-3">
+  <h1 class="text-2xl font-app">{$boardStore?.title}</h1>
+  <span class="px-2.5 py-1.5 bg-gray-100 border text-sm leading-2 rounded">
+    Members: {$boardStore?.memberCount}
+  </span>
+</header>
