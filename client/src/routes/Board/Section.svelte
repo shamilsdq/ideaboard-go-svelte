558aaf0144da2f6c5d087bce90117c35fe5acb6e
Shamil Siddique <shamilsdq@live.com>
2024-07-13 02:46:20 +0530

    Created section component for board page

---
+++ client/src/routes/Board/Section.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { CirclePlusSolid } from "svelte-awesome-icons";
+  import { boardStore } from "@/stores/board";
+
+  export let section;
+
+  $: posts = $boardStore.posts?.filter((post) => post.sectionId === section.id);
+
+  const addPost = () => {
+    console.log(`new post for section: ${section.title} (${section.id})`);
+  };
+</script>
+
+<section class="flex-1 min-w-80 flex flex-col bg-gray-100 rounded-sm border">
+  <div class="w-full flex gap-5 p-3.5 items-start">
+    <h3 class="flex-1 font-app font-medium leading-6">{section.title}</h3>
+    <div title="Add new post" class="rounded-full">
+      <CirclePlusSolid
+        withEvents
+        size="20"
+        role="button"
+        class="rounded-full duration-100 hover:scale-110 focus:outline-2 focus:outline-offset-2"
+        ariaLabel="Hello World"
+        on:click={addPost}
+      />
+    </div>
+  </div>
+  <div class="flex-1 flex flex-col overflow-y-auto">
+    {#each posts as post}
+      {post.id}
+    {/each}
+  </div>
+</section>
