0c13a3648a48887f9c7659cd2b1a071787f57b11
Shamil Siddique <shamilsdq@live.com>
2024-07-14 06:59:32 +0530

    Added sorting by id for sections and posts

--- client/src/stores/board.js
+++ client/src/stores/board.js
@@ -1,29 +1,40 @@
 import { writable } from "svelte/store";
 
+const _process = (data) => {
+  data.sections.sort((a, b) => a.id - b.id);
+  data.posts.sort((a, b) => a.id - b.id);
+  return data;
+};
+
+const _handle = (code, content, data) => {
+  switch (code) {
+    case "INITIAL":
+      return { ...content };
+    case "MEMBER_JOINED":
+    case "MEMBER_EXITED":
+      return { ...data, ...content };
+    case "POST_CREATED":
+      return { ...data, posts: [...data.posts, content] };
+    case "POST_UPDATED":
+      return {
+        ...data,
+        posts: data.posts.map((p) => (p.id === content.id ? content : p)),
+      };
+    case "POST_DELETED":
+      return {
+        ...data,
+        posts: [...data.posts.filter((p) => p.id !== content.id)],
+      };
+    default:
+      console.log("Unknown action:", { code, content });
+      return data;
+  }
+};
+
 const handle = (code, content, board) =>
   board.update((data) => {
-    switch (code) {
-      case "INITIAL":
-        return content;
-      case "MEMBER_JOINED":
-      case "MEMBER_EXITED":
-        return { ...data, ...content };
-      case "POST_CREATED":
-        return { ...data, posts: [...data.posts, content] };
-      case "POST_UPDATED":
-        return {
-          ...data,
-          posts: data.posts.map((p) => (p.id === content.id ? content : p)),
-        };
-      case "POST_DELETED":
-        return {
-          ...data,
-          posts: [...data.posts.filter((p) => p.id !== content.id)],
-        };
-      default:
-        console.log("Unknown action:", { code, content });
-        return data;
-    }
+    const updatedData = _handle(code, content, data);
+    return _process(updatedData);
   });
 
 const createBoard = () => {
